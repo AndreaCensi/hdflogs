22010a800f669c572a50cd8d328e1de12ba3192f
Andrea Censi <andrea@cds.caltech.edu>
2013-08-04 18:04:04 +0200

    Started new repo

---
+++ setup.py
@@ -0,0 +1,47 @@
+import os
+from setuptools import setup, find_packages
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+version = "1.0dev1"
+
+
+setup(
+    name='hdflogs',
+    author="Andrea Censi",
+    author_email="andrea@cds.caltech.edu",
+    url='http://github.com/AndreaCensi/hdflogs',
+    version=version,
+    description="Simple logging of timestamped numpy data in HDF",
+
+    long_description=read('README.md'),
+    keywords="",
+    license="LGPL",
+
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering',
+        'License :: OSI Approved :: GNU Library or '
+        'Lesser General Public License (LGPL)',
+    ],
+
+    package_dir={'':'src'},
+    packages=find_packages('src'),
+    entry_points={
+     'console_scripts': [
+       # 'rawlogs = rawlogs.programs:rawlogs_main'
+      ]
+    },
+    install_requires=[
+        'PyContracts',
+        'rawlogs',
+        'DecentLogs'
+    ],
+
+    tests_require=['nose']
+)
+
